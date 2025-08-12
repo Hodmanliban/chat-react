d72c0355095aafa244c62a4563b83f21b59de896
Hodman Liban <Hodman_libanh@hotmail.com>
2025-08-12 18:56:17 +0200

    changes to Auth.js

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,26 +1,38 @@
 import api from "../services/api";
 
+async function getCsrfToken() {
+  const response = await api.patch("/csrf");
+  return response.data.csrfToken;
+}
+
 // register user
 export async function register({ username, password, email, avatar }) {
-    try {
-        const response = await api.post("/auth/register", {
-            username,
-            password,
-            email,
-            avatar,
-        });
-        return response.data;
-    } catch (error) {
-        throw error.response?.data || { message: "något gick fel vid registrering." };
-    }
+  try {
+    const csrfToken = await getCsrfToken();
+    const response = await api.post("/auth/register", {
+      username,
+      password,
+      email,
+      avatar,
+      csrfToken,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "något gick fel vid registrering." };
+  }
 }
 
 // login user
 export async function login({ username, password }) {
-    try {
-        const response = await api.post("/auth/token", { username, password });
-        return response.data; // förväntar att innehåller token
-    } catch (error) {
-        throw error.response?.data || { message: "något gick fel vid inloggning." };
-    }
+  try {
+    const csrfToken = await getCsrfToken();
+    const response = await api.post("/auth/token", {
+      username,
+      password,
+      csrfToken,
+    });
+    return response.data; // förväntar att innehåller token
+  } catch (error) {
+    throw error.response?.data || { message: "något gick fel vid inloggning." };
+  }
 }
